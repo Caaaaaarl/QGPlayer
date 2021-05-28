cb82ebd36a5575923768b08c72dc122a10812094
hushiqing <hushiqing@qgutech.com>
2021-05-28 14:13:24 +0800

    update_2_20210528

---
+++ ui/src/main/java/com/google/android/exoplayer2/ui/BasePlayerFragment.java
@@ -0,0 +1,35 @@
+package com.google.android.exoplayer2.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+public class BasePlayerFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EventBus.getDefault().register(this);
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(QGBean bean) {
+        if ("full".equals(bean.tag)){
+            Intent intent = new Intent(getContext(), LandScapeActivity.class);
+            intent.putExtra("current_position",bean.position);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
+    }
+}
